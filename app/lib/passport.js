c418131a6f9d962c009a09d697a7b5c711adfe9d
Jeffrey Hunter <sublet@me.com>
2020-06-25 22:17:42 -0400

    Removing LDAP stuff

--- app/lib/passport.js
+++ app/lib/passport.js
@@ -1,12 +1,13 @@
 const passport = require('passport');
 const JWTstrategy = require('passport-jwt').Strategy;
 const ExtractJWT = require('passport-jwt').ExtractJwt;
-const ldap = require('ldapjs');
-const CustomStrategy = require('passport-custom').Strategy;
+// const ldap = require('ldapjs');
+// const ldapEscape = require('ldap-escape');
+// const CustomStrategy = require('passport-custom').Strategy;
 
 const { private: { userService } } = require('@pathcheck/data-layer')
 
-const ldapServerUrl = `ldap://${process.env.LDAP_HOST}:${process.env.LDAP_PORT}`;
+// const ldapServerUrl = `ldap://${process.env.LDAP_HOST}:${process.env.LDAP_PORT}`;
 
 const opts = {
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
@@ -35,79 +36,89 @@ const jwtStrategy = new JWTstrategy(opts, async (jwt_payload, done) => {
 
 passport.use('jwt', jwtStrategy);
 
-console.log('[LDAP] CreateClient')
-const ldapClient = ldap.createClient({
-  url: ldapServerUrl,
-});
+// const ldapClient = ldap.createClient({
+//   url: ldapServerUrl,
+// });
 
-ldapClient.on('error', err => {
-  console.log('[LDAP] on Error', err)
-  if (err.message.startsWith('connect ECONNREFUSED')) {
-    throw new Error(`LDAP server not found at ${ldapServerUrl}. Please start a server to enable authentication. Please see README.md for more information.`);
-  } else {
-    console.error(err);
-  }
-});
+// ldapClient.on('error', err => {
+//   if (err.message.startsWith('connect ECONNREFUSED')) {
+//     throw new Error(
+//       `LDAP server not found at ${ldapServerUrl}. Please start a server to enable authentication. Please see README.md for more information.`,
+//     );
+//   } else {
+//     console.error(err);
+//   }
+// });
 
-ldapClient.bind(process.env.LDAP_BIND, process.env.LDAP_PASS, err => {
-  console.log('[LDAP] bind outside')
-  if (err) console.log(err);
-});
+// ldapClient.bind(process.env.LDAP_BIND, process.env.LDAP_PASS, err => {
+//   if (err) console.log(err);
+// });
 
-/**
- * Validate the filter
- */
+// /**
+//  * Validate the filter
+//  */
 
-if (
-  process.env.LDAP_SEARCH.indexOf('{{username}}') === -1
-) {
-  console.log('[LDAP] error thrown')
-  throw new Error(
-    'LDAP_FILTER environment variable must contain the keyword {{username}}. ' +
-    'These keywords will be replaced by the request details appropriately.'
-  )
-}
+// if (process.env.LDAP_FILTER.indexOf('{{username}}') === -1) {
+//   throw new Error(
+//     'LDAP_FILTER environment variable must contain the keyword {{username}}. ' +
+//       'These keywords will be replaced by the request details appropriately.',
+//   );
+// }
 
-passport.use('ldap', new CustomStrategy(
-  function(req, done) {
-    /*
-     * Filter will look like
-     * (&(cn={{username}})(password={{password}}))
-     * {{username}} will be replaced by the sent username
-     * {{password}} will be replaced by the sent password
-     */
-    
-    console.log('[LDAP] custom strategy')
+// passport.use(
+//   'ldap',
+//   new CustomStrategy(function (req, done) {
+//     /*
+//      * Filter will look like
+//      * (&(cn={{username}})(objectClass=person))
+//      * {{username}} will be replaced by the sent username
+//      */
 
-    let query =
-      process.env.LDAP_SEARCH
-      .replace(/{{username}}/g, req.body.username);
+//     const filter = process.env.LDAP_FILTER.replace(
+//       /{{username}}/g,
+//       ldapEscape.filter`${req.body.username}`,
+//     );
 
-    ldapClient.search(query, {
-      filter: process.env.LDAP_FILTER,
-      scope: 'base',
-      // attributes: ['dn', 'sn', 'cn']
-    }, (err, res) => {
-      res.on('searchEntry', function(entry) {
-        // Compare the retrieved password and the sent password.
-        if (entry.object.userPassword !== req.body.password) {
-          return done(null, {});
-        }
-        return done(err, entry.object);
-      });
-      res.on('error', function(err) {
-        if (process.env.NODE_ENV === 'development') console.log(err.message);
-        return done(null, {});
-      });
-    });
-  },
-));
+//     const query = process.env.LDAP_SEARCH;
 
-passport.serializeUser(function(user, done) {
+//     ldapClient.search(
+//       query,
+//       {
+//         filter,
+//         scope: 'sub',
+//       },
+//       (err, res) => {
+//         if (err) console.error(err);
+
+//         res.on('searchEntry', function (entry) {
+//           if (process.env.NODE_ENV === 'development') {
+//             console.log('[LDAP] search entry');
+//             console.log(entry.object);
+//           }
+//           // Compare the retrieved password and the sent password.
+//           if (entry.object.userPassword !== req.body.password) {
+//             return done(null, {});
+//           }
+
+//           return done(err, entry.object);
+//         });
+
+//         res.on('error', function (err) {
+//           if (process.env.NODE_ENV === 'development') {
+//             console.error(err.message);
+//           }
+//           return done(null, {});
+//         });
+//       },
+//     );
+//   }),
+// );
+
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
   done(null, user);
 });
 
